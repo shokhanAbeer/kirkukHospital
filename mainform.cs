153b78df9aaf6b4fc9b64546fa6e1d5923556200
shokhanAnwar <42439814+shokhanAnwar@users.noreply.github.com>
2018-08-22 23:38:11 +0300

    adding code for login and creating second form

---
+++ mainform.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace hospital_management2018
+{
+    public partial class mainform : Form
+    {
+        public mainform()
+        {
+            InitializeComponent();
+            textBox4.Text = DateTime.Now.ToString("yyyy/MM/dd",new System.Globalization.CultureInfo(""));
+            textBox5.Text = DateTime.Now.ToString("dddd", new System.Globalization.CultureInfo(""));
+        }
+      
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainform_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = DateTime.Now.ToString("hh : mm : ss .tt");
+            this.ActiveControl = textBox1;
+            textBox1.SelectionStart = 6;
+            textBox2.SelectionStart = 6;
+         
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+                if (textBox1.Text=="shokhan" && textBox2.Text=="shokhan97")
+                {
+                    Form1 f1 = new Form1();
+                    f1.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("please check your username or password");
+                }
+            }
+          
+       
+
+       
+    }
+}
